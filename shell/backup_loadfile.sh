3dadc91a5c868d99bb198b0002a8444f327a3f3d
Robot <920896509@qq.com>
2021-11-17 19:51:03 +0800

    Update backup_loadfile.sh

--- shell/backup_loadfile.sh
+++ shell/backup_loadfile.sh
@@ -1,14 +1,14 @@
 NAME=api-1.0.jar
 PATH=/root/algo/
 
-echo ----------File modify time: $NAME --------------
+#echo ----------File modify time: $NAME --------------
 modify_time=`stat -c %Y  $PATH$NAME`
 d1=`date '+%Y%m%d%H%M' -d @$modify_time`
-echo $d1 $modify_time
+#echo $d1 $modify_time
 
-echo ----------Process ID: $NAME --------------
+#echo ----------Process ID: $NAME --------------
 ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
-echo ----------Find pid:$ID----------------------
+#echo ----------Find pid:$ID----------------------
 
 if [ -z $ID ]; then
     echo '--------No java proces running------------------'
@@ -21,8 +21,8 @@ if [ -z $ID ]; then
     exit 0
 else
     start_time=`stat -c %Y /proc/$ID`
-    echo $ID  $start_time
-    echo $modify_time
+    #echo $ID  $start_time
+    #echo $modify_time
 
     if [ $start_time -lt $modify_time ]; then
         cp $PATH$NAME $PATH${d1}"_"${NAME}
